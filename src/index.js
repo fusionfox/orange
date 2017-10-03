234a29d7ccb43372d3ede85857d1abe2b2656d76
Matthew Woolstencroft <fusionfox.uk@gmail.com>
2017-10-03 22:02:19 +0100

    Switch to HashRouter for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { HashRouter, Route } from 'react-router-dom'
 import './index.css';
 import Search from './js/Search';
 import Asset from './js/Asset';
 
 ReactDOM.render((
-  <BrowserRouter>
+  <HashRouter>
     <div>
       <Route path='/search' component={Search} />
       <Route path='/asset/:id' component={Asset} />
     </div>
-  </BrowserRouter>
+  </HashRouter>
 ), document.getElementById('root'));
