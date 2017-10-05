df9ae9144c6573fc7f49cf7b7a8778ea7a0fcc72
Matthew Woolstencroft <fusionfox.uk@gmail.com>
2017-10-05 07:52:33 +0100

    Adding BEM to Title

--- src/js/components/Title.js
+++ src/js/components/Title.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames';
 import '../../css/components/Title.css';
 
-const Title = ({text}) => {
+const Title = (props) => {
   return (
-    <h1 className='Title'>{text}</h1>
+    <h1 className={classNames(props.className, 'Title')}>
+      {props.text}
+    </h1>
   )
 }
 
