9d25500cad271b062e52f41bcca65475fca71820
Matthew Woolstencroft <fusionfox.uk@gmail.com>
2017-10-07 22:37:50 +0100

    Remove SearchControls as separate component Embed them in Search component to make use of state

--- src/js/Search.js
+++ src/js/Search.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import './../css/Search.css'
+import './../css/components/SearchControls.css'
 import Title from './components/Title'
-import SearchControls from './components/SearchControls'
 import ImageGrid from './components/ImageGrid'
 import nasaImageDataAdapter from './utils/nasaImageDataAdapter'
 
@@ -12,7 +12,8 @@ class Search extends Component {
   constructor() {
     super()
     this.state = {
-      images: []
+      images: [],
+      media_type: 'image'
     }
   }
 
@@ -25,11 +26,37 @@ class Search extends Component {
       });
   }
 
+  handleKeyUp = (e) => {
+    if (e.key === 'Enter') {
+      this.searchNasa(e.target.value)
+    }
+  }
+
+  handleRadioChange = (e) => {
+    this.setState({
+      media_type: e.target.value,
+    })
+  }
+
   render() {
     return (
       <div className="Search">
         <Title className="Search__Title" text='Search Page' />
-        <SearchControls className="Search__Controls" performSearch={this.searchNasa}/>
+        <div className='Search__Controls SearchControls'>
+          <div className='SearchControls__query'>
+            <input type='text' placeholder= 'Input query then press Enter' onKeyUp={this.handleKeyUp} />
+          </div>
+          <div className='SearchControls__filters'>
+            <label>
+              <input type='radio' name='media_type' value='image' checked={this.state.media_type === 'image'} onChange={this.handleRadioChange} />
+              Images
+            </label>
+            <label>
+              <input type='radio' name='media_type' value='audio' checked={this.state.media_type === 'audio'} onChange={this.handleRadioChange} />
+              Audio
+            </label>
+          </div>
+        </div>
         <ImageGrid className="Search__ImageGrid" images={this.state.images} />
       </div>
     );
